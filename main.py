7d052c62cb6ae341a1ea9c9b03534bc67653c32f
gainax2k1 <gainax2k1@gmail.com>
2025-01-09 02:42:34 -0800

    Made a black screen!

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame, constants
+from constants import *
 
 def main():
 	print("Starting asteroids!")
+	print("Screen width:", SCREEN_WIDTH)
+	print("Screen height:", SCREEN_HEIGHT)
+
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+			
+		screen.fill((0,0,0), rect=None, special_flags=0)
+		pygame.display.flip()
+
 
 if __name__ == "__main__":
 	main()
