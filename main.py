9ef8ae36e19b06e5c7d84d911ded7c7fc5c9ccab
gainax2k1 <gainax2k1@gmail.com>
2025-01-09 03:08:41 -0800

    adding player and circles

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
 
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
@@ -18,6 +20,7 @@ def main():
 				return
 			
 		screen.fill((0,0,0), rect=None, special_flags=0)
+		dt = clock.tick(60) / 1000
 		pygame.display.flip()
 
 
