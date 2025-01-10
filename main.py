3c4dc20b37dc97f71475d1271b537d07dc8dfc1f
gainax2k1 <gainax2k1@gmail.com>
2025-01-09 23:43:34 -0800

    draws a triangle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame, constants
+import pygame, constants, circleshape, player
 from constants import *
+from player import *
 
 def main():
 	print("Starting asteroids!")
@@ -13,13 +14,18 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	x = (SCREEN_WIDTH / 2)
+	y = (SCREEN_HEIGHT / 2) 
+	my_player = Player(x, y) 
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-			
+		
+		
 		screen.fill((0,0,0), rect=None, special_flags=0)
+		my_player.draw(screen)
 		dt = clock.tick(60) / 1000
 		pygame.display.flip()
 
--- player.py
+++ player.py
@@ -1,11 +1,21 @@
-class Player(CircleShape)
-    rotation = 0
+import circleshape, constants
+from constants import *
+from circleshape import *
 
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS) # L5 CH2 step 3
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2) # L5 CH2 step 5
+ 
     # in the player class
+    # DONT TOUCH BELOW THIS
     def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
